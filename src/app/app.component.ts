f4ec76c613f657455095e930a894a7f717546af0
parthsapra <parthsapra91@gmail.com>
2020-01-27 12:53:02 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,24 @@ import { FormBuilder,FormGroup,Validators } from "@angular/forms"
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent  implements OnInit {
   user:YahooUser = new YahooUser();
+  userFormGroup:FormGroup;
+  constructor(private formBuilder:FormBuilder){}
+
+  ngOnInit(){
+    this.userFormGroup=this.formBuilder.group({
+        firstName:['',Validators.required],
+  lastName:['',Validators.required],
+  email:['',Validators.required],
+  password:['',Validators.required],
+  day:['',Validators.required],
+  month:['',Validators.required],
+  year:['',Validators.required],
+  mobileNo:['',Validators.required],
+  gender:['',Validators.required]
+    })
+  }
   submit(){
     console.log(this.user);
     if(this.user.firstName==""){
